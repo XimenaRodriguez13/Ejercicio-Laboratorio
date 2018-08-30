1ed818d827b747a4cc681c988a970fb018586b72
Xime <xime_rdz_glz@hotmail.com>
2018-08-30 18:04:33 -0500

    Ejercicio completo

--- Alumno.java
+++ Alumno.java
@@ -1,11 +1,22 @@
-import java.util.ArrayList;
-
 public class Alumno
 {
-   
+   private String alumno;
+   private int clave;
     
-    public Alumno()
-    {
-        
-    }
+   public Alumno(String alumno,int clave)
+   {
+       this.alumno=alumno;
+       this.clave=clave;
+   }
+    
+   public int dimeClave()
+   {
+       return clave;
+   }
+   
+   public String toString()
+   {
+       String aux="Nombre:"+alumno+" Clave:"+clave;
+       return aux;
+   }
 }
--- Laboratorio.java
+++ Laboratorio.java
@@ -11,7 +11,26 @@ public class Laboratorio
    
    public void inscribir(String nombre,int clave)
    {
-       Alumno nuevo=new Alumno();
+       Alumno nuevo=new Alumno(nombre,clave);
        alumnos.add(nuevo);
+   }
+   
+   public void bajas(int claveBaja)
+   {
+       for(int i=0;i<alumnos.size();i++){
+           Alumno aux=alumnos.get(i);
+           if(aux.dimeClave()==claveBaja){
+               alumnos.remove(i);
+           }
+       }
+   }
+   
+   public void imprimir()
+   {
+        System.out.println("Lista de Alumnos");
+        for(int i=0;i<alumnos.size();i++){
+            Alumno lista=alumnos.get(i);
+            System.out.println(lista);
+        }
    }
 }
